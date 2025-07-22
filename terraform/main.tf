eb52b98c157ee746648b6ac4cbd7e1db7fa9912c
Abdul Syed <action@github.com>
2025-07-21 21:21:21 -0400

    Add int and unit test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 # terraform/main.tf
 module "s3" {
-  source = "./modules/s3"
+  source         = "./modules/s3"
   s3_bucket_name = var.s3_bucket_name
 }
 
 module "dynamodb" {
-  source = "./modules/dynamodb"
+  source     = "./modules/dynamodb"
   table_name = var.dynamodb_table_name
   aws_region = var.aws_region
 }
